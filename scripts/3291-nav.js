2876fb93cff3ef037bd94f5153c30ec136f726da
3291design - Architect <bikewales99@gmail.com>
2026-05-11 12:53:24 +0100

    Update navigation links in 3291-nav.js

--- scripts/3291-nav.js
+++ scripts/3291-nav.js
@@ -4,10 +4,10 @@ const masterNavContainer = document.getElementById('master-nav-anchor');
 const navHTML = `
 <nav id="master-nav-2026">
     <a href="https://3291design.github.io/">Home</a>
+    <a href="https://3291design.github.io/ourfamilytree/">Our Family Tree</a>
+    <a href="https://3291design.github.io/oakdalewarmemorial/">Oakdale War Memorial</a>
     <a href="https://3291design.github.io/bikewales/">Bike Wales</a>
-    <a href="https://3291design.github.io/curtisfieldsdesign/">Portfolio</a>
-    <a href="https://3291design.github.io/oakdalewarmemorial/">Archive</a>
-    <a href="https://3291design.github.io/ourfamilytree/">Family Tree</a>
+    <a href="https://3291design.github.io/curtisfieldsdesign/">Architect's Studio</a>
     <a href="https://3291design.github.io/techlab/">Tech Lab</a>
 </nav>
 `;
